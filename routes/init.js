52e2a8ba2f59071bf0c077c0e9c0bdc2a374beed
qiru <bjqiru@corp.netease.com>
2018-01-09 14:09:42 +0800

    添加动画

--- routes/init.js
+++ routes/init.js
@@ -1,5 +1,6 @@
 'use strict';
 var yule = require('./pages/yule');
+var yule2017 = require('./pages/yule2017');
 var trail = require('./pages/trail');
 var run = require('./pages/run');
 var perticle = require('./pages/particle');
@@ -9,6 +10,7 @@ var canvasLeaf = require('./pages/canvasLeaf');
 
 module.exports = function(app) {
    app.use('/yule', yule);
+   app.use('/yule2017', yule2017);
    app.use('/trail', trail);
    app.use('/run', run);
    app.use('/particle', perticle);
